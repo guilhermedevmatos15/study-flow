953e7659139701ec9927bce85949c4d7249ca4ee
Guilherme Matos <devguilhermeferreira@gmail.com>
2025-04-29 20:12:15 -0300

    build: Criação dos arquivos

---
+++ src/pages/Error/ErrorPage.tsx
@@ -0,0 +1,11 @@
+import { FunctionComponent } from "react";
+
+const ErrorPage: FunctionComponent = () => {
+	return (
+		<div className="ErrorPage">
+			<h1>Error Page</h1>
+		</div>
+	);
+};
+
+export default ErrorPage;
